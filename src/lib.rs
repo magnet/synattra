6348340bc93959cfcf9c1f7cb72c86949915b84b
Simon Chemouil <simon.chemouil@lambdacube.fr>
2019-02-06 18:15:31 +0100

    Some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,15 @@
+//! # A Syn Attribute Parser Toolkit
+//! 
+//! Synattra extends Syn and provides structures to easily parse custom attributes.
+//! 
+//! Notably, Synattra provides a `KVOption<K, V>` type that allows parsing attributes in the form `key = value` where `key` can be any token or custom keyword (including Rust keywords!) and value any type that can be parsed from a `TokenStream`. Synattra was extracted from the [Metered project](https://github.com/magnet/metered-rs), please check it for actual usage.
+//! 
+//! Synattra also supports single or multiple values, that can take the shape of `Foo` or `[Foo, Bar]`.
+//! 
+//! Finally Synattra provides some extra types, such as `InvokePath` which represents any invocation handle, macro or not (e.g `foo` or `println!`).
+//! 
+//! By reusing Syn's design, Synattra parsers are very robust and when they compile, they usually work :-).
+
 #[macro_use]
 extern crate syn;
 
